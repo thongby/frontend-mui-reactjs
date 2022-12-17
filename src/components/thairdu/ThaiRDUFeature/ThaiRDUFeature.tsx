d5941a98ae7be66bbf759652ef8242bbe7aa860e
Thong Boonyot <thongby@gmail.com>
2022-12-17 14:08:08 +0700

    Update ThaiRDU components

--- src/components/thairdu/ThaiRDUFeature/ThaiRDUFeature.tsx
+++ src/components/thairdu/ThaiRDUFeature/ThaiRDUFeature.tsx
@@ -1,11 +1,32 @@
 import * as React from "react";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 type ThaiRDUFeatureProps = {
   //
 };
 
 const ThaiRDUFeature: React.FC<any> = () => {
-  return <div>ThaiRDUFeature</div>;
-};
-
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Item>ความสามารถของโปรแกรม ThaiRDU</Item>
+        </Grid>
+      </Grid>
+    </Box>
+    </>
+  );
+  };
 export default ThaiRDUFeature;
--- src/components/thairdu/ThaiRDUQA/ThaiRDUQA.tsx
+++ src/components/thairdu/ThaiRDUQA/ThaiRDUQA.tsx
@@ -1,11 +1,33 @@
 import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 type ThaiRDUQAProps = {
   //
 };
 
 const ThaiRDUQA: React.FC<any> = () => {
-  return <div>ThaiRDUQA</div>;
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Item>คำถามที่พบบ่อยของโปรแกรม ThaiRDU</Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
 };
 
 export default ThaiRDUQA;
